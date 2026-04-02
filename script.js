8b8e2481828fc94f768d00e1460e6fe8d7c3a249
Leosardo14 <leonardo_delvecchio@icloud.com>
2026-04-02 08:56:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function calcola(op) {
   let n1 = parseFloat(document.getElementById("num1").value);
   let n2 = parseFloat(document.getElementById("num2").value);
-
   let risultato;
 
-  if (op === "+") risultato = n1 + n2;
-  if (op === "-") risultato = n1 - n2;
-  if (op === "*") risultato = n1 * n2;
-  if (op === "/") risultato = n1 / n2;
+  if(op === "+") risultato = n1 + n2;
+  if(op === "-") risultato = n1 - n2;
+  if(op === "*") risultato = n1 * n2;
+  if(op === "/") risultato = n1 / n2;
 
   document.getElementById("risultato").innerText = "Risultato: " + risultato;
 }
